09c817be07abd32e570b627327f063e4945e50cf
IsabellaAngelC <148303246+IsabellaAngelC@users.noreply.github.com>
2024-02-09 17:55:02 -0500

    Add files via upload

---
+++ Quiz 1/index.js
@@ -0,0 +1,16 @@
+const studentGrades = [100, 50, 65, 70, 85, 96, 45];
+const result = computeGrades(studentGrades);
+console.log(result);
+
+studentGrades.forEach((grade) => {
+	if (grade > 90 && grade < 100) {
+		return 'A';
+		consolele.log(computeGrades);
+	}
+});
+
+const A = passed;
+const B = passed;
+const C = passed;
+const D = passed;
+const F = lose;
